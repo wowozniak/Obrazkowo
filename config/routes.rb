9481f8d361d5581ebe1ba6b059fbb2335d08faf5
Wowozniak <wojciechszwozniak@gmail.com>
2018-02-11 15:19:56 -0500

    Trzecie wyslanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-root 'home#index'
+root to: 'home#index'
 get 'home/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
